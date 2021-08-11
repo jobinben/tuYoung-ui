e754b218fe3b4ccb9945becb2e0a82a6bea54e80
大冰 <1021137079@qq.com>
2021-08-11 16:53:44 +0800

    新增ButtonGroup组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from "vue"
 import Button from "./components/Button.vue"
 import Icon from "./components/Icon.vue"
+import ButtonGroup from "./components/ButtonGroup.vue"
 
 Vue.component('t-button', Button)
 Vue.component('t-icon', Icon)
+Vue.component('t-button-group', ButtonGroup)
 
 
 
---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="t-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped >
+.t-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+
+  > .t-button {
+    border-radius: 0;
+    margin-left: -1px; /**解决边框重叠 */
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 1; /**解决边框重叠 */
+    }
+  }
+}
+</style>
