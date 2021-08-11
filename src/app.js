131bdf557b9646e1f431df29801d0e92a8b23622
大冰 <1021137079@qq.com>
2021-08-11 23:13:54 +0800

    新增4个测试用例，第5个测试用例有问问题

--- src/app.js
+++ src/app.js
@@ -21,17 +21,119 @@ new Vue({
 import chai from "chai"
 
 const expect = chai.expect
-// 单元测试
+// 单元测试 (1) t-button的icon属性
 {
     const Construtor = Vue.extend(Button)
-    const button = new Construtor({
+    const vm = new Construtor({
         propsData: { //该属性可以传入button组件的属性
             icon: 'settings'
         }
     })
 
-    button.$mount('#test')
-    let useElement = button.$el.querySelector('use')
+    // button.$mount('#test') //挂载dom上测试
+    vm.$mount() // 非css测试可以挂载内存测试
+
+    let useElement = vm.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
     expect(href).to.eq('#i-settings')
+
+    // 测试完后销毁button
+    vm.$el.remove()
+    vm.$destroy()
+}
+// 单元测试 (2) t-button的loading属性
+{
+    const Construtor = Vue.extend(Button)
+    const vm = new Construtor({
+        propsData: { //该属性可以传入button组件的属性
+            icon: 'settings',
+            loading: true
+        }
+    })
+
+    vm.$mount() //挂载在内存中测试
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    // 测试完后销毁button
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+// 单元测试 (3) t-button的iconPosition属性
+{
+    // 测试css需要创建div挂载测试 因为css需要挂载了才能触发
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+
+    const Construtor = Vue.extend(Button)
+    const vm = new Construtor({
+        propsData: {
+            icon: 'settings',
+        }
+    })
+
+    vm.$mount(div)
+
+    let svg = vm.$el.querySelector('svg')
+    // let order = window.getComputedStyle('svg').order
+    let {order} = window.getComputedStyle(svg)
+
+    expect(order).to.eq('1')
+    
+    // 测试完后销毁button
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+// 单元测试 (4) t-button的iconPosition属性
+{
+    // 测试css需要创建div挂载测试 因为css需要挂载了才能触发
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+
+    const Construtor = Vue.extend(Button)
+    const vm = new Construtor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+
+    vm.$mount(div)
+
+    let svg = vm.$el.querySelector('svg')
+    // let order = window.getComputedStyle('svg').order
+    let {order} = window.getComputedStyle(svg)
+
+    expect(order).to.eq('2')
+    
+    // 测试完后销毁button
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+
+// 单元测试 (5) t-button的click点击事件
+{
+    
+
+    const Construtor = Vue.extend(Button)
+    const vm = new Construtor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+
+    vm.$mount()
+    vm.$on('click', function() {
+        // 可以被点击就会触发此次
+        console.log('可以被点击')
+        expect(1).to.eq(1)  // 错误写法的测试
+    })
+    // 希望button可以被点击
+    let button = vm.$el
+    button.click()
+  
 }
