304d19b02510fb8f51c5aefaabe1fcaf99eb1079
大冰 <1021137079@qq.com>
2021-08-11 17:42:30 +0800

    t-button-group的第一个元素不需要margin-left

--- src/components/ButtonGroup.vue
+++ src/components/ButtonGroup.vue
@@ -5,16 +5,18 @@
 </template>
 
 <script>
-export default {  
-    mounted() {
-        // 检测子元素是不是全是button
-        for (let node of this.$el.children) { 
-            let name = node.nodeName.toLowerCase()
-            if(name !== 'button') {
-                console.warn(`t-button-group 的子元素应该全是t-button, 但你写的是 ${name}。可能会造成显示问题。`)
-            }
-        }
+export default {
+  mounted() {
+    // 检测子元素是不是全是button
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== "button") {
+        console.warn(
+          `t-button-group 的子元素应该全是t-button, 但你写的是 ${name}。可能会造成显示问题。`
+        );
+      }
     }
+  },
 };
 </script>
 
@@ -25,7 +27,9 @@ export default {
 
   > .t-button {
     border-radius: 0;
-    margin-left: -1px; /**解决边框重叠 */
+    &:not(:first-child) {
+      margin-left: -1px; /**解决边框重叠 */
+    }
     &:first-child {
       border-top-left-radius: var(--border-radius);
       border-bottom-left-radius: var(--border-radius);
