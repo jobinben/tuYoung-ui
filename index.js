84ecc71e6319b05e9f795f252fefdba1d53dc74f
大冰 <1021137079@qq.com>
2021-08-14 13:20:45 +0800

    接入npm发布配置

---
+++ index.js
@@ -0,0 +1,9 @@
+import TButton from "./src/components/Button"
+import TButtonGroup from "./src/components/ButtonGroup"
+import TIcon from "./src/components/Icon"
+
+export {
+    TButton,
+    TButtonGroup,
+    TIcon
+}
