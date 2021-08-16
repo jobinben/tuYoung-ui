54a70a534c86eb2515f8fb09139a61d6265df975
大冰 <1021137079@qq.com>
2021-08-17 00:30:43 +0800

    优化input测试用例

--- tests/unit/TInput.spec.js
+++ tests/unit/TInput.spec.js
@@ -16,129 +16,78 @@ describe('Input:', () => { // 检测元素
     })
 
     describe('Props:', () => { // 检测属性
-        
-        it('可以设置value.', () => {
-            const wrapper = mount(Input, {
-                propsData: {
-                    value: 'testValue'
-                }
-            })
-            const tValue = wrapper.props().value
-            expect(tValue).to.eq('testValue')
-            wrapper.destroy()
-        })
 
-        it('可以设置placeholder.', () => {
-            const wrapper = mount(Input, {
+        let wrapper
+        // mocha的hook函数 runs before each test in this block
+        beforeEach( () => {
+            wrapper = mount(Input, {
                 propsData: {
-                    placeholder: 'testPlaceholder'
-                }
-            })
-            const tPlaceholder = wrapper.props().placeholder
-            expect(tPlaceholder).to.eq('testPlaceholder')
-            wrapper.destroy()
-        })
-
-        it('可以设置error.', () => {
-            const wrapper = mount(Input, {
-                propsData: {
-                    error: 'testError'
-                }
-            })
-            const tError = wrapper.text()
-            expect(tError).to.eq('testError')
-            wrapper.destroy()
-        })
-
-        it('可以设置success.', () => {
-            const wrapper = mount(Input, {
-                propsData: {
-                    success: 'testSuccess'
-                }
-            })
-            const tSuccess = wrapper.text()
-            expect(tSuccess).to.eq('testSuccess')
-            wrapper.destroy()
-        })
-
-        it('可以设置type.', () => {
-            const wrapper = mount(Input, {
-                propsData: {
-                    type: 'number'
-                }
-            })
-            const tType = wrapper.props().type
-            expect(tType).to.eq('number')
-            wrapper.destroy()
-        })
-
-        it('可以设置disabled.', () => {
-            const wrapper = mount(Input, {
-                propsData: {
+                    value: 'testValue',
+                    placeholder: 'testPlaceholder',
+                    error: 'testError',
+                    success: 'testSuccess',
+                    type: 'number',
                     disabled: ''
                 }
             })
+        })
+        // runs after each test in this block
+        afterEach( () => {
+            wrapper.destroy()
+        })
+
+        it('可以设置value.', () => {
+            const tValue = wrapper.props().value
+            expect(tValue).to.eq('testValue')
+        })
+
+        it('可以设置placeholder.', () => {
+            const tPlaceholder = wrapper.props().placeholder
+            expect(tPlaceholder).to.eq('testPlaceholder')
+        })
+
+        it('可以设置error.', () => {
+            const tError = wrapper.props().error
+            expect(tError).to.eq('testError')
+        })
+
+        it('可以设置success.', () => {
+            const tSuccess = wrapper.props().success
+            expect(tSuccess).to.eq('testSuccess')
+        })
+
+        it('可以设置type.', () => {
+            const tType = wrapper.props().type
+            expect(tType).to.eq('number')
+        })
+
+        it('可以设置disabled.', () => {
             const tDisabled = wrapper.props().disabled
             expect(tDisabled).to.eq(true)
-            wrapper.destroy()
         })
 
     })
 
 
     describe('Events:', () => { // 检测事件
-        
-        it('可以触发change事件', () => {
-            const wrapper = mount(Input, {})
-            const vm = wrapper.vm
-            const callback = Sinon.fake()
+        const wrapper = mount(Input, {}).vm
+        // mocha提供的hook函数, 每一个test的测试好之后运行
+        afterEach( () => {
+            wrapper.$destroy()
+        })
+        // 优化测试用例
+        it('支持 change/input/focus/blur 事件.', () => {   
+            let eventList = ['change', 'input', 'focus', 'blur']
+            eventList.forEach( eventName => {
+                const callback = Sinon.fake()
+                const event = new Event(eventName) // 创建事件
+                wrapper.$on(eventName, callback) // 添加监听事件
+                const inputElement = wrapper.$el.querySelector('input') // 找到目标元素
+                inputElement.dispatchEvent(event) // 派遣事件，也就是调用事件
+                // 事件调用call的断言需要sinon-chai
+                expect(callback).to.have.been.calledWith(event)
+            })
             
-
-            const event = new Event('change') // 创建事件
-            vm.$on('change', callback) // 添加监听事件
-            const inputElement = vm.$el.querySelector('input') // 找到目标元素
-            inputElement.dispatchEvent(event) // 派遣事件，也就是调用事件
-            // 事件调用call的断言需要sinon-chai
-            expect(callback).to.have.been.calledWith(event)
-        })
-
-        it('可以触发input事件', () => {
-            const wrapper = mount(Input, {})
-            const vm = wrapper.vm
-            const callback = Sinon.fake()
-
-            const event = new Event('input') // 创建事件
-            vm.$on('input', callback)  // 添加监听事件
-            const inputElement = vm.$el.querySelector('input') // 找到目标元素
-            inputElement.dispatchEvent(event) // 派遣事件，也就是调用事件
-            // 事件调用call的断言需要sinon-chai
-            expect(callback).to.have.been.calledWith(event)
-        })
-
-        it('可以触发focus事件', () => {
-            const wrapper = mount(Input, {})
-            const vm = wrapper.vm
-            const callback = Sinon.fake()
-
-            const event = new Event('focus') // 创建事件
-            vm.$on('focus', callback)  // 添加监听事件
-            const inputElement = vm.$el.querySelector('input') // 找到目标元素
-            inputElement.dispatchEvent(event) // 派遣事件，也就是调用事件
-            // 事件调用call的断言需要sinon-chai
-            expect(callback).to.have.been.calledWith(event)
-        })
-
-        it('可以触发blur事件', () => {
-            const wrapper = mount(Input, {})
-            const vm = wrapper.vm
-            const callback = Sinon.fake()
-
-            const event = new Event('blur') // 创建事件
-            vm.$on('blur', callback)  // 添加监听事件
-            const inputElement = vm.$el.querySelector('input') // 找到目标元素
-            inputElement.dispatchEvent(event) // 派遣事件，也就是调用事件
-            // 事件调用call的断言需要sinon-chai
-            expect(callback).to.have.been.calledWith(event)
         })
         
     })
