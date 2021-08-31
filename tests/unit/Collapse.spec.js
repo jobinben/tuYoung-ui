da5ebbac5b10186a52a4490950c446e1f685e896
大冰 <1021137079@qq.com>
2021-08-31 17:04:44 +0800

    增加Collapse测试用例

---
+++ tests/unit/Collapse.spec.js
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils'
+import Collapse from "../../src/components/collapse/Collapse"
+import CollapseItem from "../../src/components/collapse/CollapseItem"
+import chai from "chai"
+import Sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
+chai.use(sinonChai)
+
+const expect = chai.expect
+
+describe('Collapse:', () => { // 检测元素
+
+    it('Collapse 存在.', () => {
+        expect(Collapse).to.be.exist
+    })
+
+    it(' CollapseItem 存在.', () => {
+        expect(CollapseItem).to.be.exist
+    })
+
+
+
+    describe('Props', () => {
+       
+
+    })
+
+})
