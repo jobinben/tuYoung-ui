050ab0f82b127bf4c6f6a18c9183b60ee16d8f36
jobinben <1021137079@qq.com>
2021-09-02 20:00:50 +0800

    导出所有组件

--- index.js
+++ index.js
@@ -2,11 +2,42 @@ import Button from "./src/components/button/Button"
 import ButtonGroup from "./src/components/button/ButtonGroup"
 import Collapse from "./src/components/collapse/Collapse"
 import CollapseItem from "./src/components/collapse/CollapseItem"
-
+import Column from "./src/components/grid/Column"
+import Row from "./src/components/grid/Row"
 import Icon from "./src/components/icon/Icon"
+import Input from "./src/components/input/Input"
+import Layout from "./src/components/layout/Layout"
+import Header from "./src/components/layout/Header"
+import Content from "./src/components/layout/Content"
+import Footer from "./src/components/layout/Footer"
+import Sider from "./src/components/layout/Sider"
+import Popover from "./src/components/popover/Popover"
+import Tabs from "./src/components/tabs/tabs"
+import TabsHead from "./src/components/tabs/tabs-head"
+import TabsBody from "./src/components/tabs/tabs-body"
+import TabsItem from "./src/components/tabs/tabs-item"
+import TabsPane from "./src/components/tabs/tabs-pane"
+import ToastPlugin from "./src/components/toast/toastPlugin"
 
 export {
     Button,
-    TButtonGroup,
-    Icon
+    ButtonGroup,
+    Collapse,
+    CollapseItem,
+    Column,
+    Row,
+    Icon,
+    Input,
+    Layout,
+    Header,
+    Content,
+    Footer,
+    Sider,
+    Popover,
+    Tabs,
+    TabsHead,
+    TabsBody,
+    TabsItem,
+    TabsPane,
+    ToastPlugin
 }
