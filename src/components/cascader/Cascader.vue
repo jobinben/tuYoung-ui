e193e1c38b537ffb3b6b12fb066a3ea00ea33e29
jobinben <1021137079@qq.com>
2021-09-09 18:41:39 +0800

    创建cascader级联组件

---
+++ src/components/cascader/Cascader.vue
@@ -0,0 +1,19 @@
+<!-- author: 大冰 -->
+<template>
+  <div class="cascader">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TCascader",
+
+}
+</script>
+<style lang='scss' scoped>
+  @import "../../assets/css/style.scss";
+  .cascader {
+
+  }
+</style>
