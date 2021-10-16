71acb579b7271cac5fce0832a773070a533d64d1
jobinben <1021137079@qq.com>
2021-10-16 21:36:39 +0800

    官网部署到nginx

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     head: [
         ["link", { rel: 'icon', href: 'logo.png' }]
     ],
-    base: '/tuYoung-ui/',
+    base: '/',
     title: 'tuYoung UI',
     description: '一个基于Vue.2x的UI框架',
     themeConfig: {
